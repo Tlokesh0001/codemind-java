f66a307e095d64f901f60654ce729069d718addf
Tlokesh0001 <106659982+Tlokesh0001@users.noreply.github.com>
2022-10-04 22:42:20 +0530

    import java.util.*; class VAMPIRE {     public static void main(String[] args) {         Scanner in = new Scanner(System.in);         int r = in.nextInt();         System.out.printf("%.2f",r*r*3.14);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class VAMPIRE {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int r = in.nextInt();
+        System.out.printf("%.2f",r*r*3.14);
+    }
+}
