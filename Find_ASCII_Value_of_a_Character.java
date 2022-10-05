288c800a252a652c5294758394caa561db5f9be3
Tlokesh0001 <106659982+Tlokesh0001@users.noreply.github.com>
2022-10-05 20:35:38 +0530

    import java.util.*; class VAMPIRE {     public static void main(String[] args) {         Scanner in = new Scanner(System.in);         char c = in.nextLine().charAt(0);         System.out.println((int)(c));     } }

--- Find_ASCII_Value_of_a_Character.java
+++ Find_ASCII_Value_of_a_Character.java
@@ -2,7 +2,7 @@ import java.util.*;
 class VAMPIRE {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        char ch = in.nextLine().charAt(0);
-        System.out.println((int)(ch));
+        char c = in.nextLine().charAt(0);
+        System.out.println((int)(c));
     }
 }
